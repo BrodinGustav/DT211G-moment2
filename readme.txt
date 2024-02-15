3b2caa226fd50fd3d5dddd82f5bdcb4ed15a5bed
Gustav Brodin <brodin.gustav@gmail.com>
2024-02-15 08:24:55 +0100

    Skapat js-funktion för sortering och utskrift av API

--- readme.txt
+++ readme.txt
@@ -3,6 +3,8 @@
 -- 2024-02-12-- installerat uglifyjs
 -- 2024-02-12-- installerat htmlnano
 -- 2024-02-12-- ordnat mappstruktur
-
 -- 2024-02-12-- ordnat html-layout
 -- 2024-02-12-- ordnat css
+
+--2024-02-15-- Hämtat API 
+--2024-02-15-- Skapat js-funktion för sortering och utskrift av API
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", fetchData);
 
+//Hämta API
 async function fetchData() {
   try {
     const response = await fetch('https://webbutveckling.miun.se/files/ramschema_ht23.json');
@@ -10,13 +11,40 @@ async function fetchData() {
   }
 }
 
-
+//Hämtar ID
 function populateTable(data) {
     const tableBody = document.getElementById('tableBody');
   
+    //Loopar igenom och skriver ut till tableBody
     data.forEach(course => {
       const row = document.createElement('tr');
       row.innerHTML = `<td>${course.code}</td><td>${course.coursename}</td><td>${course.progression}</td>`;
       tableBody.appendChild(row);
     });
   }
+
+  //Funktion för sortering
+  function sortTable(columnIndex) {
+    
+    //Hämta in table som ska sorteras
+    const table = document.getElementById('courseTable');
+    
+    //Hämtar in <tbody>-element
+    const tbody = table.getElementsByTagName('tbody'); 
+    
+    //Hämtar in <tr>-element, konventerar till array för sort-metod
+    const rows = Array.from(tbody.getElementsByTagName('tr')); 
+
+    rows.sort((a, b) => {
+        //Hämtar text från <td>-element från klickad kolumn
+        const textA = a.getElementsByTagName('td')[columnIndex].textContent;
+        const textB = b.getElementsByTagName('td')[columnIndex].textContent;
+        //Jämför text, sortera rader och ordna i stigande ordning
+        return textA.localeCompare(textB);
+      });
+      
+      //Skriv ut till HTML
+      tbody.innerHTML = '';
+      rows.forEach(row => tbody.appendChild(row));
+    }
+    
